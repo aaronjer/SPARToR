d705a7bd4123b4be6d33ad4e79ca5dbc6678aedf
Jer Wilson <realaddress@legiturl.com>
2010-02-22 02:02:00 -0800

    now five player models, visually aim guns

--- mod.c
+++ mod.c
@@ -55,42 +55,44 @@ void mod_setvideo(int w,int h) {
   surf_player = IMG_Load("images/player.png");
 
   // scale up?
-  if( scale>1 ) {
-    SDL_Surface *surf_tmp;
-    Uint32 key;
-    Uint8 r,g,b;
-    surf_tmp = SJDL_CopyScaled(surf_player, SDL_HWSURFACE|SDL_SRCCOLORKEY, scale);
-    SDL_FreeSurface(surf_player);
-    SJDL_GetPixel(surf_tmp,0,0,&r,&g,&b); //assume upper-leftmost pixel is transparent color
-    key = SDL_MapRGB(surf_tmp->format,r,g,b);
-    SDL_SetColorKey(surf_tmp,SDL_SRCCOLORKEY,key);
-    surf_player = SDL_DisplayFormat(surf_tmp);
-    SDL_FreeSurface(surf_tmp);
-  }
+  SDL_Surface *surf_tmp;
+  Uint32 key;
+  Uint8 r,g,b;
+  surf_tmp = SJDL_CopyScaled(surf_player, SDL_HWSURFACE|SDL_SRCCOLORKEY, scale);
+  SDL_FreeSurface(surf_player);
+  SJDL_GetPixel(surf_tmp,0,0,&r,&g,&b); //assume upper-leftmost pixel is transparent color
+  key = SDL_MapRGB(surf_tmp->format,r,g,b);
+  SDL_SetColorKey(surf_tmp,SDL_SRCCOLORKEY,key);
+  surf_player = SDL_DisplayFormat(surf_tmp);
+  SDL_FreeSurface(surf_tmp);
 }
 
 void mod_draw(SDL_Surface *screen,int objid,OBJ_t *o) {
   Uint32 color;
-  char txtbuf[80];
   //SDL_Rect srect = (SDL_Rect){0,0,200,200};
   SDL_Rect drect;
   switch(o->type)
   {
     case OBJT_PLAYER: {
       PLAYER_t *pl = o->data;
-      color = SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF);
+      int gunshift;
+      if( pl->goingu ) gunshift = 96;
+      if( pl->goingd ) gunshift = 48;
+      if( !(pl->goingu^pl->goingd) ) gunshift = 0;
       drect = (SDL_Rect){(pl->pos.x-10),(pl->pos.y-15),0,0};
       if( pl->facingr ) {
-        SJDL_BlitScaled(surf_player, &(SDL_Rect){ 0, 0,20,30}, screen, &drect, scale);
-        drect = (SDL_Rect){(pl->pos.x- 5-pl->gunback),(pl->pos.y-5+pl->gundown/5),0,0};
-        SJDL_BlitScaled(surf_player, &(SDL_Rect){20, 0,24,12}, screen, &drect, scale);
+        if( pl->model==4 ) //girl hair
+          SJDL_BlitScaled(surf_player, &(SDL_Rect){ 80,120,20,15}, screen, &(SDL_Rect){drect.x-4,drect.y+pl->gundown/7,0,0},scale);
+        SJDL_BlitScaled(surf_player, &(SDL_Rect){ 0, 0+pl->model*30,20,30}, screen, &drect, scale);
+        drect = (SDL_Rect){(pl->pos.x- 5-pl->gunback),(pl->pos.y-10+pl->gundown/5),0,0};
+        SJDL_BlitScaled(surf_player, &(SDL_Rect){ 0+gunshift,150,24,21}, screen, &drect, scale);
       } else {
-        SJDL_BlitScaled(surf_player, &(SDL_Rect){ 0,30,20,30}, screen, &drect, scale);
-        drect = (SDL_Rect){(pl->pos.x-19+pl->gunback),(pl->pos.y-5+pl->gundown/5),0,0};
-        SJDL_BlitScaled(surf_player, &(SDL_Rect){20,12,24,12}, screen, &drect, scale);
+        if( pl->model==4 ) //girl hair
+          SJDL_BlitScaled(surf_player, &(SDL_Rect){100,120,20,15}, screen, &(SDL_Rect){drect.x+4,drect.y+pl->gundown/7,0,0},scale);
+        SJDL_BlitScaled(surf_player, &(SDL_Rect){20, 0+pl->model*30,20,30}, screen, &drect, scale);
+        drect = (SDL_Rect){(pl->pos.x-19+pl->gunback),(pl->pos.y-10+pl->gundown/5),0,0};
+        SJDL_BlitScaled(surf_player, &(SDL_Rect){24+gunshift,150,24,21}, screen, &drect, scale);
       }
-      sprintf(txtbuf,"%d",objid);
-      SJF_DrawText(screen, pl->pos.x*scale, pl->pos.y*scale, txtbuf);
       break;
     }
     case OBJT_BULLET: {
@@ -130,122 +132,122 @@ void mod_adv(Uint32 objid,Uint32 a,Uint32 b,OBJ_t *oa,OBJ_t *ob) {
         slot0 = findfreeslot(b);
         slot1 = findfreeslot(b);
         SJC_Write("%d: New client %d detected, created ghost is obj#%d, player is obj#%d",
-                  hotfr,objid,i,slot0,slot1);
-        fr[b].objs[slot0].type = OBJT_GHOST;
-        fr[b].objs[slot0].flags = OBJF_POS;
-        fr[b].objs[slot0].size = sizeof(GHOST_t);
-        gh = fr[b].objs[slot0].data = malloc(sizeof(GHOST_t));
-        gh->pos = (V){0.0f,0.0f,0.0f};
-        gh->client = i;
-        gh->avatar = slot1;
-        fr[b].objs[slot1].type = OBJT_PLAYER;
-        fr[b].objs[slot1].flags = OBJF_POS|OBJF_VEL|OBJF_HULL|OBJF_PVEL|OBJF_VIS|OBJF_PLAT|OBJF_CLIP;
-        fr[b].objs[slot1].size = sizeof(PLAYER_t);
-        pl = fr[b].objs[slot1].data = malloc(sizeof(PLAYER_t));
-        pl->pos  = (V){200.0f,200.0f,0.0f};
-        pl->vel  = (V){0.0f,0.0f,0.0f};
-        pl->hull[0] = (V){-8.0f,-15.0f,0.0f};
-        pl->hull[1] = (V){ 8.0f, 15.0f,0.0f};
-        pl->pvel = (V){0.0f,0.0f,0.0f};
-        pl->model = 1;
-        pl->ghost = slot0;
-        pl->goingl = 0;
-        pl->goingr = 0;
-        pl->goingu = 0;
-        pl->goingd = 0;
-        pl->jumping = 0;
-        pl->firing = 0;
-        pl->cooldown = 0;
-        pl->projectiles = 0;
-        pl->grounded = 0;
-        pl->facingr = 1;
-      }
+                hotfr,i,slot0,slot1);
+      fr[b].objs[slot0].type = OBJT_GHOST;
+      fr[b].objs[slot0].flags = OBJF_POS;
+      fr[b].objs[slot0].size = sizeof(GHOST_t);
+      gh = fr[b].objs[slot0].data = malloc(sizeof(GHOST_t));
+      gh->pos = (V){0.0f,0.0f,0.0f};
+      gh->client = i;
+      gh->avatar = slot1;
+      fr[b].objs[slot1].type = OBJT_PLAYER;
+      fr[b].objs[slot1].flags = OBJF_POS|OBJF_VEL|OBJF_HULL|OBJF_PVEL|OBJF_VIS|OBJF_PLAT|OBJF_CLIP;
+      fr[b].objs[slot1].size = sizeof(PLAYER_t);
+      pl = fr[b].objs[slot1].data = malloc(sizeof(PLAYER_t));
+      pl->pos  = (V){200.0f,200.0f,0.0f};
+      pl->vel  = (V){0.0f,0.0f,0.0f};
+      pl->hull[0] = (V){-8.0f,-15.0f,0.0f};
+      pl->hull[1] = (V){ 8.0f, 15.0f,0.0f};
+      pl->pvel = (V){0.0f,0.0f,0.0f};
+      pl->model = i%5;
+      pl->ghost = slot0;
+      pl->goingl = 0;
+      pl->goingr = 0;
+      pl->goingu = 0;
+      pl->goingd = 0;
+      pl->jumping = 0;
+      pl->firing = 0;
+      pl->cooldown = 0;
+      pl->projectiles = 0;
+      pl->grounded = 0;
+      pl->facingr = 1;
     }
+  }
+  break;
+case OBJT_GHOST:
+  assert("ob->size==sizeof(GHOST_t)",ob->size==sizeof(GHOST_t));
+  gh = ob->data;
+  break;
+case OBJT_DUMMY:
+  assert("ob->size==sizeof(DUMMY_t)",ob->size==sizeof(DUMMY_t));
+  DUMMY_t *du = ob->data;
+
+  // friction
+  if(      du->vel.x> 0.1f ) du->vel.x -= 0.1f;
+  else if( du->vel.x>-0.1f ) du->vel.x  = 0.0f;
+  else                       du->vel.x += 0.1f;
+
+  if( objid==(hotfr+100)%2000 ) //tee-hee
+    du->vel.x += (float)(b%4)-1.5;
+
+  du->vel.y += 0.8f;        //gravity
+  break;
+case OBJT_PLAYER:
+  assert("ob->size==sizeof(PLAYER_t)",ob->size==sizeof(PLAYER_t));
+  PLAYER_t *oldme = oa->data;
+  PLAYER_t *newme = ob->data;
+  gh = fr[b].objs[newme->ghost].data;
+  switch( fr[b].cmds[gh->client].cmd ) {
+    case CMDT_1LEFT:  newme->goingl  = 1; newme->facingr = 0; break;
+    case CMDT_0LEFT:  newme->goingl  = 0;                     break;
+    case CMDT_1RIGHT: newme->goingr  = 1; newme->facingr = 1; break;
+    case CMDT_0RIGHT: newme->goingr  = 0;                     break; 
+    case CMDT_1UP:    newme->goingu  = 1;                     break;
+    case CMDT_0UP:    newme->goingu  = 0;                     break;
+    case CMDT_1DOWN:  newme->goingd  = 1;                     break;
+    case CMDT_0DOWN:  newme->goingd  = 0;                     break; 
+    case CMDT_1JUMP:  newme->jumping = 1;                     break;
+    case CMDT_0JUMP:  newme->jumping = 0;                     break;
+    case CMDT_1FIRE:  newme->firing  = 1;                     break;
+    case CMDT_0FIRE:  newme->firing  = 0;                     break;
+  }
+
+  if( !oldme ) //FIXME why's this null?
     break;
-  case OBJT_GHOST:
-    assert("ob->size==sizeof(GHOST_t)",ob->size==sizeof(GHOST_t));
-    gh = ob->data;
-    break;
-  case OBJT_DUMMY:
-    assert("ob->size==sizeof(DUMMY_t)",ob->size==sizeof(DUMMY_t));
-    DUMMY_t *du = ob->data;
 
-    // friction
-    if(      du->vel.x> 0.1f ) du->vel.x -= 0.1f;
-    else if( du->vel.x>-0.1f ) du->vel.x  = 0.0f;
-    else                       du->vel.x += 0.1f;
+  newme->gunback = 0; //reset gun position
+  if(newme->goingr||newme->goingl)
+    newme->gundown = (newme->gundown+1)%10;
+  else
+    newme->gundown = 0;
 
-    if( objid==(hotfr+100)%2000 ) //tee-hee
-      du->vel.x += (float)(b%4)-1.5;
+  // friction
+  if(      newme->vel.x> 0.2f ) newme->vel.x -= 0.2f;
+  else if( newme->vel.x>-0.2f ) newme->vel.x  = 0.0f;
+  else                          newme->vel.x += 0.2f;
+  if(      newme->pvel.x> 0.5f ) newme->pvel.x -= 0.5f;
+  else if( newme->pvel.x>-0.5f ) newme->pvel.x  = 0.0f;
+  else                           newme->pvel.x += 0.5f;
 
-    du->vel.y += 0.8f;        //gravity
-    break;
-  case OBJT_PLAYER:
-    assert("ob->size==sizeof(PLAYER_t)",ob->size==sizeof(PLAYER_t));
-    PLAYER_t *oldme = oa->data;
-    PLAYER_t *newme = ob->data;
-    gh = fr[b].objs[newme->ghost].data;
-    switch( fr[b].cmds[gh->client].cmd ) {
-      case CMDT_1LEFT:  newme->goingl  = 1; newme->facingr = 0; break;
-      case CMDT_0LEFT:  newme->goingl  = 0;                     break;
-      case CMDT_1RIGHT: newme->goingr  = 1; newme->facingr = 1; break;
-      case CMDT_0RIGHT: newme->goingr  = 0;                     break; 
-      case CMDT_1UP:    newme->goingu  = 1;                     break;
-      case CMDT_0UP:    newme->goingu  = 0;                     break;
-      case CMDT_1DOWN:  newme->goingd  = 1;                     break;
-      case CMDT_0DOWN:  newme->goingd  = 0;                     break; 
-      case CMDT_1JUMP:  newme->jumping = 1;                     break;
-      case CMDT_0JUMP:  newme->jumping = 0;                     break;
-      case CMDT_1FIRE:  newme->firing  = 1;                     break;
-      case CMDT_0FIRE:  newme->firing  = 0;                     break;
-    }
+  // -- WALK --
+  if( newme->goingl ) {
+    if(      newme->pvel.x>-2.0f ) newme->pvel.x += -1.0f;
+    else if( newme->pvel.x>-3.0f ) newme->pvel.x  = -3.0f;
+  }
+  if( newme->goingr ) {
+    if(      newme->pvel.x< 2.0f ) newme->pvel.x +=  1.0f;
+    else if( newme->pvel.x< 3.0f ) newme->pvel.x  =  3.0f;
+  }
 
-    if( !oldme ) //FIXME why's this null?
-      break;
+  // -- JUMP --
+  if( newme->pvel.y <= -2.0f ) {     //jumping in progress
+    newme->pvel.y   +=  2.0f;        //jumpvel fades into real velocity
+    newme->vel.y    += -2.0f;
+  }
+  else if( newme->pvel.y < 0.0f ) {  //jumping ending
+    newme->vel.y    += newme->pvel.y;
+    newme->pvel.y   = 0.0f;
+    newme->jumping  = 0;             //must press jump again now
+  }
+  if( !newme->jumping )              //low-jump, cancel jump velocity early
+    newme->pvel.y   = 0.0f;
+  if( (newme->vel.y==0.0f || oldme->vel.y==0.0f) && newme->jumping ) //FIXME 0 velocity means grounded? not really
+    newme->pvel.y  = -11.9f;         //initiate jump!
 
-    newme->gunback = 0; //reset gun position
-    if(newme->goingr||newme->goingl)
-      newme->gundown = (newme->gundown+1)%10;
-    else
-      newme->gundown = 0;
-
-    // friction
-    if(      newme->vel.x> 0.2f ) newme->vel.x -= 0.2f;
-    else if( newme->vel.x>-0.2f ) newme->vel.x  = 0.0f;
-    else                          newme->vel.x += 0.2f;
-    if(      newme->pvel.x> 0.5f ) newme->pvel.x -= 0.5f;
-    else if( newme->pvel.x>-0.5f ) newme->pvel.x  = 0.0f;
-    else                           newme->pvel.x += 0.5f;
-
-    // -- WALK --
-    if( newme->goingl ) {
-      if(      newme->pvel.x>-2.0f ) newme->pvel.x += -1.0f;
-      else if( newme->pvel.x>-3.0f ) newme->pvel.x  = -3.0f;
-    }
-    if( newme->goingr ) {
-      if(      newme->pvel.x< 2.0f ) newme->pvel.x +=  1.0f;
-      else if( newme->pvel.x< 3.0f ) newme->pvel.x  =  3.0f;
-    }
-
-    // -- JUMP --
-    if( newme->pvel.y <= -2.0f ) {     //jumping in progress
-      newme->pvel.y   +=  2.0f;        //jumpvel fades into real velocity
-      newme->vel.y    += -2.0f;
-    }
-    else if( newme->pvel.y < 0.0f ) {  //jumping ending
-      newme->vel.y    += newme->pvel.y;
-      newme->pvel.y   = 0.0f;
-      newme->jumping  = 0;             //must press jump again now
-    }
-    if( !newme->jumping )              //low-jump, cancel jump velocity early
-      newme->pvel.y   = 0.0f;
-    if( (newme->vel.y==0.0f || oldme->vel.y==0.0f) && newme->jumping ) //FIXME 0 velocity means grounded? not really
-      newme->pvel.y  = -11.9f;         //initiate jump!
-
-    // -- FIRE --
-    if( newme->cooldown>0 )
-      newme->cooldown--;
-    if( newme->firing && newme->cooldown==0 && newme->projectiles<3 ) { // create bullet
+  // -- FIRE --
+  if( newme->cooldown>0 )
+    newme->cooldown--;
+  if( newme->firing && newme->cooldown==0 && newme->projectiles<5 ) { // create bullet
       slot0 = findfreeslot(b);
       fr[b].objs[slot0].type = OBJT_BULLET;
       fr[b].objs[slot0].flags = OBJF_POS|OBJF_VEL|OBJF_VIS;
@@ -258,16 +260,20 @@ void mod_adv(Uint32 objid,Uint32 a,Uint32 b,OBJ_t *oa,OBJ_t *ob) {
         bu->pos = (V){newme->pos.x-19.0f,newme->pos.y-3.0f,0.0f};
         bu->vel = (V){-8.0f,0.0f,0.0f};
       }
-      if( newme->goingu ) // aiming
-        bu->vel.y +=-8.0f;
-      if( newme->goingd )
-        bu->vel.y += 8.0f;
+      if( newme->goingu ) { // aiming
+        bu->vel.y += -8.0f;
+        bu->pos.y +=-10.0f;
+      }
+      if( newme->goingd ) {
+        bu->vel.y +=  8.0f;
+        bu->pos.y += 12.0f;
+      }
       bu->model = 1;
       bu->owner = objid;
       bu->ttl = 50;
       newme->cooldown = 5;
       newme->projectiles++;
-      newme->gunback = 1;
+      newme->gunback = 2;
     }
 
     for(i=0;i<objid;i++)  //find other players to interact with -- who've already MOVED
